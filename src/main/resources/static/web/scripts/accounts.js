9ba68f0e1ff449065f563f4c51dcdd3d7bd34d8c
GabiBarbera <gabriel.barberaa@gmail.com>
2023-08-06 17:18:35 -0300

    reduccion de codigo en accounts.js

--- src/main/resources/static/web/scripts/accounts.js
+++ src/main/resources/static/web/scripts/accounts.js
@@ -7,10 +7,6 @@ createApp({
         return {
             accounts: [],
             clients: [],
-            firstName: '',
-            lastName: '',
-            email: '',
-            jsonRest: null,
         }
     },
     created() {
@@ -23,28 +19,6 @@ createApp({
                     this.clients = response.data
                     this.accounts = response.data.accounts
                     this.jsonRest = JSON.stringify(response.data, null, 1);
-                })
-                .catch(error => console.error('Error:', error));
-        },
-        inputCheck() {
-            if (this.firstName && this.lastName && this.email) {
-                this.addClient();
-            } else {
-                window.alert('Please complete the form');
-            }
-        },
-        addClient() {
-            let clientNew = {
-                firstName: this.firstName,
-                lastName: this.lastName,
-                email: this.email,
-            }
-            axios.post(url, clientNew)
-                .then(response => {
-                    this.firstName = '';
-                    this.lastName = '';
-                    this.email = '';
-                    this.loadData();
                 })
                 .catch(error => console.error('Error:', error));
         }
