a21a7d899fb2bafc68b42e5e12ee1634c3083a7f
GabiBarbera <gabriel.barberaa@gmail.com>
2023-08-09 13:08:44 -0300

    Arreglo de peticion a la api- url en account

--- src/main/resources/static/web/scripts/account.js
+++ src/main/resources/static/web/scripts/account.js
@@ -1,11 +1,11 @@
 const { createApp } = Vue
 
-const url = 'http://localhost:8080/api/accounts'
+const url = 'http://localhost:8080/api/accounts/'
 
 createApp({
     data() {
         return {
-            accountId: [],
+            account: [],
             transactions: [],
         }
     },
@@ -14,14 +14,18 @@ createApp({
     },
     methods: {
         loadData() {
-            axios.get(url)
+            const parameter = location.search
+            const parameters = new URLSearchParams(parameter)
+            const idParameters = parameters.get("id")
+            axios.get(url + idParameters)
                 .then(response => {
-                    const parameter = location.search
-                    const parameters = new URLSearchParams(parameter)
-                    const idParameters = parameters.get("id")
-                    const allAccounts = response.data
-                    this.accountId = allAccounts.find(idAccount => idAccount.id == idParameters)
-                    this.transactions = this.accountId.transactionDTOSet.sort((a, b) => b.id - a.id)
+                    this.account = response.data
+                    console.log(this.account);
+                    this.transactions = this.account.transactionDTOSet
+                    console.log(this.transactions);
+                    // const account = response.data
+                    // this.accountId = allAccounts.find(idAccount => idAccount.id == idParameters)
+                    // this.transactions = this.accountId.transactionDTOSet.sort((a, b) => b.id - a.id)
                 })
                 .catch(error => console.error('Error:', error));
         }
