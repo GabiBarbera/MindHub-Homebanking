f23b8140dd7fa2d9e46f103620f47f377d1b999f
GabiBarbera <gabriel.barberaa@gmail.com>
2023-08-28 18:24:20 -0300

    arreglo en clientcontroller

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -29,7 +29,7 @@ public class ClientController {
     private String randomNumber(){
         String random;
         do {
-            int number = (int)(Math.random()*100+999);
+            int number = (int)(Math.random() * 99999999 + 1);
             random = "VIN-" + number;
         } while (accountRepository.findByNumber(random)!=null);
         return random;
