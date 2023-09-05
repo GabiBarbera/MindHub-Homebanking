9320bd83f38a9be8192947498baeb31aa5839910
GabiBarbera <gabriel.barberaa@gmail.com>
2023-09-05 16:19:21 -0300

    arreglos en el back

--- src/main/java/com/mindhub/homebanking/controllers/LoanController.java
+++ src/main/java/com/mindhub/homebanking/controllers/LoanController.java
@@ -4,18 +4,15 @@ package com.mindhub.homebanking.controllers;
 import com.mindhub.homebanking.dtos.LoanApplicationDTO;
 import com.mindhub.homebanking.dtos.LoanDTO;
 import com.mindhub.homebanking.models.*;
-import com.mindhub.homebanking.repositories.ClientLoanRepository;
-import com.mindhub.homebanking.repositories.ClientRepository;
-import com.mindhub.homebanking.repositories.LoanRepository;
 import com.mindhub.homebanking.repositories.TransactionRepository;
 import com.mindhub.homebanking.services.AccountService;
+import com.mindhub.homebanking.services.ClientLoanService;
 import com.mindhub.homebanking.services.ClientService;
 import com.mindhub.homebanking.services.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,8 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/api")
@@ -37,7 +32,7 @@ public class LoanController {
     @Autowired
     AccountService accountService;
     @Autowired
-    ClientLoanRepository clientLoanRepository;
+    ClientLoanService clientLoanService;
     @Autowired
     TransactionRepository transactionRepository;
 
@@ -50,42 +45,48 @@ public class LoanController {
     @RequestMapping(path = "/loans", method = RequestMethod.POST)
     public ResponseEntity<Object> newLoan(Authentication authentication, @RequestBody LoanApplicationDTO loanApplicationDTO) {
         Client client = clientService.findByEmail(authentication.getName());
-
         if (client == null) {
-            return new ResponseEntity<>("Client no found", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("Client no found.", HttpStatus.FORBIDDEN);
         }
         Loan loan = loanService.findById(loanApplicationDTO.getId());
-        if (loan.getPayments() == null) {
-            return new ResponseEntity<>("Number of payments not found", HttpStatus.FORBIDDEN);
-        }
         if (loan == null) {
-            return new ResponseEntity<>("The requested loan does not exist", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("The requested loan does not exist.", HttpStatus.FORBIDDEN);
+        }
+        if (loan.getPayments() == null) {
+            return new ResponseEntity<>("Number of payments not found.", HttpStatus.FORBIDDEN);
         }
         if (loan.getMaxAmount() <= 0) {
-            return new ResponseEntity<>("The requested amount cannot be zero", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("The requested amount cannot be zero.", HttpStatus.FORBIDDEN);
         }
         if (loan.getMaxAmount() < loanApplicationDTO.getAmount()) {
-            return new ResponseEntity<>("The amount requested cannot exist", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("The amount requested cannot exist.", HttpStatus.FORBIDDEN);
         }
         if (!loan.getPayments().contains(loanApplicationDTO.getPayments())) {
-            return new ResponseEntity<>("The chosen payment is not available", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("The chosen payment is not available.", HttpStatus.FORBIDDEN);
+        }
+        if (loanApplicationDTO.getNumberAccountDestination() == null) {
+            return new ResponseEntity<>("Account destination no exist.",HttpStatus.FORBIDDEN);
         }
         Account account = accountService.findByNumber(loanApplicationDTO.getNumberAccountDestination());
         if (account == null) {
-            return new ResponseEntity<>("Destination account does not exist", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("Destination account does not exist.", HttpStatus.FORBIDDEN);
+        }
+        if (!client.getAccounts().contains(account)){
+            return new ResponseEntity<>("The account does not belong to an authenticated client",HttpStatus.FORBIDDEN);
         }
         if (account.getOwner().getId() != client.getId()) {
-            return new ResponseEntity<>("This account does not belong to you", HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>("This account does not belong to you.", HttpStatus.UNAUTHORIZED);
         }
         account.setBalance(account.getBalance() + loanApplicationDTO.getAmount());
         ClientLoan clientLoan = new ClientLoan(loanApplicationDTO.getAmount() * 1.2, loanApplicationDTO.getPayments());
         clientLoan.setClient(client);
         clientLoan.setLoan(loan);
-        clientLoanRepository.save(clientLoan);
-        String transactionDescription = loan.getName() + "Loan approved";
+        clientLoanService.addClientLoan(clientLoan);
+        String transactionDescription = loan.getName() + "Loan approved.";
         Transaction transaction = new Transaction(loanApplicationDTO.getAmount(), transactionDescription, LocalDateTime.now(), TransactionType.CREDIT);
         transactionRepository.save(transaction);
         accountService.addAccount(account);
+        account.addTransaction(transaction);
         return new ResponseEntity<>("Loan request created successfully", HttpStatus.CREATED);
     }
 }
